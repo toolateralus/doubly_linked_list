441872dacb5bf2dfec7b1cd383e338fe991addb5
Your Name <joshnjoshgaming@gmail.com>
2024-03-06 20:54:15 -0500

    added insert and improved 'at'

--- list.h
+++ list.h
@@ -10,13 +10,15 @@ typedef struct node_t {
   struct node_t *previous;
   void *data;
 } Node;
-typedef struct list_t {
+
+typedef struct {
   Node *head;
   Node *tail;
   int count;
   int sizeof_data;
 } List;
-static List *list_new(int sizeof_data) {
+
+static List *list_new(const int sizeof_data) {
   assert(sizeof_data != 0);
   List *list = (List *)malloc(sizeof(List));
   list->sizeof_data = sizeof_data;
@@ -46,50 +48,102 @@ static void *pop(List *list) {
   assert(list->head != NULL);
 
   Node *current = list->tail;
-  
+
   int count = list->count;
-  
+
   list->count--;
-  
+
   if (count == 1) {
     void *data = current->data;
     free(current);
     list->head = NULL;
     return data;
   }
-  
+
   void *data = current->data;
-  list->tail =current->previous;
+  list->tail = current->previous;
   current->previous->next = NULL;
   free(current);
   return data;
 }
-static void *at(List *list, int index) {
-  if (index == 0 && list->head != NULL) {
-    return list->head->data;
+static void free_list(List *list) {
+  int i = 0;
+  while (list->count > 0) {
+    void *element = pop(list);
+    free(element);
+    i++;
   }
-  if (index == list->count - 1 && list->tail != NULL) {
-    return list->tail->data;
+  assert(list->count == 0);
+  free(list);
+  list = NULL;
+}
+static Node *node_at(List *list, int index) {
+  if (index == 0) {
+    assert(list->head != NULL);
+    return list->head;
+  }
+  if (index == list->count - 1) {
+    assert(list->tail != NULL);
+    return list->tail;
   }
   
   int current;
   Node *node = NULL;
   
-  if (list->count / 2 < index) {
-    node = list->head;
-    for (current = 0; current < index && current < list->count; ++current) {
-      node = node->next;  
+  if (index > list->count / 2) {
+    // closer to the end
+    node = list->tail;
+    for (current = list->count - 1; current > index && current > 0; --current) {
+      node = node->previous;
     }
   } else {
-    node = list->tail;
-    for (current = list->count; current > index && current > 0; --current) {
-      node = node->previous;
-    } 
+    // closer to the beginning
+    node = list->head;
+    for (current = 0; current < index && current < list->count; ++current) {
+      node = node->next;
+    }
   }
-  
   assert(current == index);
   assert(node != NULL);
+  return node;
+}
+static void *at(List *list, const int index) {
+  return node_at(list, index)->data;
+}
+static void insert(List *list, const int index, void *data) {
+  assert(index <= list->count);
+  // if index is count, which is out of bounds, we just push back.
+  if (index == list->count) {
+    push(list, data);
+    return;
+  }
   
-  return node->data;
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  new_node->data = data;
+
+  if (index == 0) {
+    // if at index 0, we don't need to setup the previous node.
+    // ?? -> current -> ??
+    Node *current = list->head;
+    // new_node <- current
+    current->previous = new_node;
+    // new_node -> current
+    new_node->next = current;
+    // establish new head of list.
+    list->head = new_node;
+  
+  } else {
+    Node *previous = node_at(list, index - 1);
+    Node *current = previous->next;
+    
+    previous->next = new_node;
+    
+    if (current != NULL)
+        current->previous = new_node;
+    
+    new_node->previous = previous;
+    new_node->next = current;
+  }
+  list->count++;
 }
 #endif
--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include "list.h"
 #include <stdio.h>
 
-
 typedef struct {
-  int a, b;
-  char c;
+  int a, b, c;
 } test_t;
-
-test_t *make_test_struct(int a, int b, char c) {
-  test_t *test = (test_t*)malloc(sizeof(test_t));
+test_t *make_test_struct(int a, int b, int c) {
+  test_t *test = (test_t *)malloc(sizeof(test_t));
   test->a = a;
   test->b = b;
   test->c = c;
@@ -17,68 +14,141 @@ test_t *make_test_struct(int a, int b, char c) {
 void test_push_pop() {
   
   char input = 0;
-  
-  printf("examine memory usage, then type '1' then press enter to fill & empty the list.\n");
-  
-  while (1) {
-    scanf("%c", &input);
+  printf("MASS_PUSH_MASS_POP_TEST\n");
     
-    if (input) {
-      break;
-    }
-  }
-  
   List *list = list_new(sizeof(test_t));
   for (int i = 0; i < 25; ++i) {
-     push(list, make_test_struct(i, i * 2, i % 255));
+    push(list, make_test_struct(i, i * 2, i % 255));
   }
-  printf("\nsizeof list: %d\n", list->count);
+  //printf("\nsizeof list: %d\n", list->count);
   
   int i = 0;
   while (list->count > 0) {
     test_t *element = pop(list);
-    printf("%d, %d, %c\n", element->a, element->b, element->c);
+    //printf("%d, %d, %c\n", element->a, element->b, element->c);
     free(element);
     i++;
   }
   
-  printf("list destroyed : %d elements\n", i);
+  //printf("list destroyed : %d elements\n", i);
   
   free(list);
-  
-  printf("examine memory usage, or type '1' and press enter to exit.\n");
-  
-  input = 0;
-  
-  while(1) {
-    scanf(" %c", &input);
-    if (input != 0) {
-      break;
-    }
-  }
+  printf("MASS_PUSH_MASS_POP PASSED\n");
 }
-
 void test_element_at() {
+  printf("INDEXING_'at()'_TEST\n");
+  
   List *list = list_new(sizeof(test_t));
-  push(list, make_test_struct(1, 2, 3));
-  push(list, make_test_struct(4, 5, 6));
-  push(list, make_test_struct(7, 8, 9));
   
+  for (int i = 0; i < 250; ++i) {
+    push(list, make_test_struct(i,i,i));
+  }
   
-  for (int i = 0; i < 3; ++i) {
-    test_t *element = (test_t *)at(list, i);
+  for (int i = 0; i < 250; ++i) {
+    test_t *element = at(list, i);
     if (element == NULL) {
-      printf ("fetched element at index %d was null", i);
+      printf("fetched element at index %d was null\n", i);
       return;
     }
-    printf("element at %d: %d, %d, %d\n",i, element->a, element->b, element->c);
+    printf("i==%d\n",i);
+    assert(element->a == i);
+    assert(element->b == i);
+    assert(element->c == i);
+  }
+  printf("INDEXEX_'at()'_TEST PASSED\n");
+}
+void test_insertion(const int insertion_index, const char* title) {
+  printf("INSERTION_TEST_AT_%s\n", title);
+  
+  List *list = list_new(sizeof(test_t));
+  for (int i = 0; i < 2500; ++i) {
+    push(list, make_test_struct(0, 0, 0));
   }
   
+  const int desired_size = list->count + 1;
+  
+  const int test_value_a = 10;
+  const int test_value_b = 11;
+  const char test_value_c = 12;
+  
+  const test_t *test_struct = make_test_struct(test_value_a, test_value_b, test_value_c);
+  
+  insert(list, insertion_index, (void*)test_struct);
+  
+  assert(list->count == desired_size);
+  
+  const test_t *element = at(list, insertion_index);
+  
+  assert(element->a == test_value_a);
+  assert(element->b == test_value_b);
+  assert(element->c == test_value_c);
+  
+}
+enum INSERTION_TEST {
+  INSERT_AT_HEAD = 0,
+  INSERT_IN_MIDDLE = 1,
+  INSERT_AT_TAIL = 5,
+};
+void test_freeing_list() {
+  printf("TEST_FREE_LIST\n");
+  List *list = list_new(sizeof(test_t));
+  for (int i = 0; i < 250; ++i) {
+    push(list, make_test_struct(0,0,0));
+  }
+  free_list(list);
+  assert(list->count == 0);
+  list = NULL;
+  printf("TEST_FREE_LIST PASSED\n");
+}
+void test_insert_at_one() {
+  List *list = list_new(sizeof(test_t));
+  
+  for (int i = 0 ; i < 10; ++i) {
+    push(list, make_test_struct(i, i, i % 255));
+  }
+  
+  test_t *test = make_test_struct(100, 100, 100);
+  
+  insert(list, 1, test);
+  
+  test_t *t1 = at(list, 1);
+  test_t *t2 = at(list, 2);
+  
+  printf("at index 1 %d,%d,%d\n", t1->a, t1->b, t1->c);
+  printf("at index 2 %d,%d,%d\n", t2->a, t2->b, t2->c);
+}
+
+void test_insert_at_1251() {
+  List *list = list_new(sizeof(test_t));
+  
+  for (int i = 0 ; i < 2500; ++i) {
+    push(list, make_test_struct(i, i, i % 255));
+  }
+  
+  test_t *test = make_test_struct(100, 100, 100);
+  
+  insert(list, 1251, test);
+  
+  test_t *t1 = at(list, 1251);
+  test_t *t2 = at(list, 1252);
+  
+  printf("at index 1251 %d,%d,%d\n", t1->a, t1->b, t1->c);
+  printf("at index 1252 %d,%d,%d\n", t2->a, t2->b, t2->c);
 }
 
 int main(int argc, char **argv) {
-  test_element_at();
   test_push_pop();
+  test_element_at();
+  test_freeing_list();
+  test_insert_at_one();
+  test_insert_at_1251();
+  test_insertion(0, "HEAD");
+  test_insertion(1, "ONE");
+  test_insertion(2, "TAIL");
+  
+   for (int i = 0; i < 2500; ++i) {
+     printf("%d\n", i);
+     test_insertion(i, "LOOP");
+   }
   return 0;
 }
-
